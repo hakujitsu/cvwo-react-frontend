f195de5fe65bbf8c41828c1dc8dac93fe63e3a41
hakujitsu <minyihkwek@gmail.com>
2020-01-10 09:22:14 +0800

    Installed uuid

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -9,13 +9,13 @@ class Sidebar extends React.Component {
     render() {
       return (
         <div className = "sidebar">
-            {/* <div className = "default_sidebar">
+            <div className = "default_sidebar">
                 <div className = "default_options">
                     <img className = "icon" src ={all} alt=""></img>
                     <span className = "sb">All</span>
                 </div>
                 
-                <div className = "default_options">
+                {/* <div className = "default_options">
                     <img className = "icon" src = {today} alt=""></img>
                     <span className = "sb">Today</span>
                 </div>
@@ -23,9 +23,9 @@ class Sidebar extends React.Component {
                 <div className = "default_options">
                     <img className = "icon" src = {thisweek} alt=""></img>
                     <span className = "sb">This Week</span>
-                </div>
+                </div> */}
 
-            </div> */}
+            </div>
             <SidebarTagConnector/>
         </div>
         );
